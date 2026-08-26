e0beff778a135e1a6d5e0def847718a50ee6e8f9
LeCatMan <161946737+LeCatMan@users.noreply.github.com>
2026-08-26 21:29:27 +0300

    one more

--- Examples/example.cpp
+++ Examples/example.cpp
@@ -1,36 +1,37 @@
 #include "BlockEngine.hpp"// we include this because we need it :D
 
-// Hover over functions/classes to see they works.
-
 int main() {
     // here we initialize the (Logger, Window, Input, Audio) and set the exit key.
-    InitializeBlockEngine(800, 600, "Block Engine", BLOCK_KEY_ESCAPE, DEFAULT_MASTER_VOLUME);
+    InitializeBlockEngine(800, 600, "Block Engine", BLOCK_KEY_ESCAPE, 1.0f);
 
     {
-        // Audio objects can be used for any object that needs audio.
+        // This is an audio object it make
         Audio AudioObject[2];
-
         AudioObject[0].LoadSound("src/Assets/BlockEngine/Audio/StartUp/start.mp3", DEFAULT_VOLUME, false, DEFAULT_PITCH);
         AudioObject[1].LoadSound("src/Assets/BlockEngine/Audio/Sounds/Correct.mp3", DEFAULT_VOLUME, false, DEFAULT_PITCH);
-        
         Triangle MyTriangle(Color(180,180,180));
         Square MySquare(Color(155,155,180));
 
         info("Entering Game Loop");
         while (!WindowShouldClose())
         {
+
             UpdateWindow();
             if(KeyEvent(BLOCK_KEY_F,BLOCK_PRESS))
             {
-                AudioObject[0].PlaySound();
+                AudioObject[0].OverlappingSound();
             }
             if(KeyEvent(BLOCK_KEY_G,BLOCK_RELEASE))
             {
-                AudioObject[1].PlaySound();
+                AudioObject[1].NoneOverlappingSound();
             }
             if(KeyEvent(BLOCK_KEY_H,BLOCK_REPEAT))
             {
-                AudioObject[1].PlaySound();
+                AudioObject[1].NoneOverlappingSound();
+            }
+            if(KeyEvent(BLOCK_KEY_Y,BLOCK_REPEAT))
+            {
+                AudioObject[0].OverlappingSound();
             }
             BackGroundColor(Color(255,255,160), 255);
             MySquare.DrawSquare();
@@ -40,6 +41,6 @@ int main() {
         info("Exited Game Loop");
     }
 
-    ShutdownBlockEngine();
+    BlockEngineShutdown();
     return 0;
 }
