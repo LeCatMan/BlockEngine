da3cdd93c1bc1fe9cb480487eb0d06e2ea8024c8
LeCatMan <161946737+LeCatMan@users.noreply.github.com>
2026-08-27 19:36:20 +0300

    test

--- BlockEngine/src/Debugger/Utilities/Utilities.cpp
+++ BlockEngine/src/Debugger/Utilities/Utilities.cpp
@@ -1,23 +1,34 @@
 #include "Utilities.hpp"
 
-char RunningPath[4096];
+
 
 char *CurrentRunningPath()
 {
+    char RunningPath[1024];
+    char *err_msg = strerror(errno);
 
-    if (getcwd(RunningPath, sizeof(RunningPath)) == NULL) {
-        perror("getcwd");
-    } else {
+    if (getcwd(RunningPath, sizeof(RunningPath)) == NULL) 
+    {
+        if (errno == ERANGE)
+        {
+            error("Path is too long! Buffer size of %zu bytes is not enough.", sizeof(RunningPath));
+        }
+        else
+        {
+            error("Failed to get current working directory: %s", strerror(errno));
+        }
+        return NULL;
+    }
+    else
+    {
+        return RunningPath;
         printf("%s\n", RunningPath);
     }
-    
-    return RunningPath;
 }
 
-
-char *GetFileText(const char *filename)
+char *GetFileText(const char *FileName)
 {
-    FILE *file = fopen(filename, "rb");
+    FILE *file = fopen(FileName, "rb");
 
     if (file == NULL)
         return NULL;
@@ -42,3 +53,17 @@ char *GetFileText(const char *filename)
 
     return text;
 }
+
+const char* BlockGetErrorString(BlockResult Result) {
+    switch (Result) {
+        case BLOCK_SUCCESS:           return "SUCCESS";
+        case BLOCK_FAILURE:           return "FAILURE";
+        case BLOCK_ERR_NULL_POINTER:  return "ERR_NULL_POINTER";
+        case BLOCK_ERR_OUT_OF_MEMORY: return "ERR_OUT_OF_MEMORY";
+        case BLOCK_ERR_FILE_IO:       return "ERR_FILE_IO";
+        case BLOCK_ERR_INVALID_PARAM: return "ERR_INVALID_PARAM";
+        case BLOCK_ERR_INIT_FAILED:   return "ERR_INIT_FAILED";
+        default:                      return "UNKNOWN_ERROR";
+    }
+}
+
--- BlockEngine/src/Debugger/Utilities/Utilities.hpp
+++ BlockEngine/src/Debugger/Utilities/Utilities.hpp
@@ -1,10 +1,29 @@
 #pragma once
-#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-#include <dirent.h>
 #include <stdlib.h>
+#include <errno.h>
+#include "../Logger/Logger.hpp"
 
+#if defined(_WIN32) || defined(_WIN64)
+    #include <direct.h>
+    #define getcwd _getcwd
+#else
+    #include <unistd.h>
+#endif
+
+typedef enum BlockResult {
+    BLOCK_SUCCESS = 0,
+    BLOCK_FAILURE = -1,          // General or unspecified failure
+    BLOCK_ERR_NULL_POINTER = -2, // Defensive check failed
+    BLOCK_ERR_OUT_OF_MEMORY = -3,// Allocation failure
+    BLOCK_ERR_FILE_IO = -4,      // Asset or file loading failed
+    BLOCK_ERR_INVALID_PARAM = -5,// Bad argument passed to function
+    BLOCK_ERR_INIT_FAILED = -6   // Subsystem initialization failed
+} BlockResult;
+
+// Fast string conversion for the logging system
+const char* BlockGetErrorString(BlockResult Result);
 
 char *CurrentRunningPath();
-char *GetFileText(const char *filename);
+char *GetFileText(const char *FileName);
